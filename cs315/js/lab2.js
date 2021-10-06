4d2ff2267c050b98f698709006b7169ef2a51c47
SibhatMan <88125004+Kgetu88@users.noreply.github.com>
2021-10-06 03:27:27 -0500

    updated

---
+++ cs315/js/lab2.js
@@ -0,0 +1,61 @@
+function add() {
+    let input = document.getElementById("textInput");
+    let output = document.getElementById("textoutput")
+    output.value = input.value + input.value;
+    input.value = "";
+
+   
+}
+
+function addition() {
+    let firstvalue = +document.getElementById("firstValue").value;
+    let secondtvalue = +document.getElementById("secondValue").value
+    let sum = firstvalue + secondtvalue;
+
+    let result = document.createElement("p");
+    result.innerHTML = sum;
+    document.body.append(result);
+
+}
+function subtraction() {
+    let firstvalue = +document.getElementById("firstValue").value;
+    let secondtvalue = +document.getElementById("secondValue").value;
+    let difference = firstvalue - secondtvalue;
+
+    let result = document.createElement("p");
+    result.innerHTML = difference;
+    document.body.append(result);
+
+}
+
+function multiplication() {
+    let firstvalue = +document.getElementById("firstValue").value;
+    let secondtvalue = +document.getElementById("secondValue").value;
+    let product = firstvalue * secondtvalue;
+
+    let result = document.createElement("p");
+    result.innerHTML = product;
+    document.body.append(result);
+
+}
+
+
+function addItem(){
+    let name =document.getElementById("name").value;
+    let catagory=document.getElementById("catagory").value;
+    let quantity=document.getElementById("quantity").value;
+    let itemtable=document.getElementById("textable");
+    let index=1;
+    let row=itemtable.insertRow(index);
+
+    let cell1=row.insertCell(0);
+    let cell2=row.insertCell(1);
+    let cell3=row.insertCell(2);
+     cell1.innerHTML=name;
+     cell2.innerHTML=catagory;
+     cell3.innerHTML=quantity;
+       document.getElementById("name").value="";
+      catagory=document.getElementById("catagory").value="";
+      quantity=document.getElementById("quantity").value=" ";
+      index++;
+}
---
+++ cs315/js/lab3.js
@@ -0,0 +1,149 @@
+var array = [];
+  var index = 0;
+
+  function addItem() {
+    index++;
+    var item = {
+      id: index, 
+      name: document.getElementById("itemName").value,
+      category: document.getElementById("category").value,
+       quantity: document.getElementById("quantity").value,
+        price:document.getElementById("price").value,
+        rate:+document.getElementById("rate").value,
+    };
+    array.push(item);
+
+
+    let items = document.getElementById("items");
+    let tr = document.createElement("tr");
+    items.append(tr);
+    let nameTd = document.createElement("td");
+    nameTd.innerHTML = document.getElementById("itemName").value;
+    tr.append(nameTd);
+
+    let categoryTd = document.createElement("td");
+    categoryTd.innerHTML = document.getElementById("category").value;
+    tr.append(categoryTd);
+
+    let quantityTd = document.createElement("td");
+    quantityTd.innerHTML = document.getElementById("quantity").value;
+    tr.append(quantityTd);
+    let priceTd = document.createElement("td");
+    priceTd.innerHTML = document.getElementById("price").value;
+    tr.append( priceTd);
+    
+      let ratingTd = document.createElement("td");
+         ratingTd.innerHTML = document.getElementById("rate").value;
+        tr.append(ratingTd);
+    
+
+    
+    let editTd = document.createElement("td");
+    let button = document.createElement("button");
+    button.innerHTML = "Edit";
+    button.setAttribute("myId", index);
+    button.onclick = function () {
+      console.log(this);
+      console.log(this.parentNode);
+      console.log(this.parentNode.parentNode);
+      console.log(this.parentNode.parentNode.childNodes);
+      let childNodes = this.parentNode.parentNode.childNodes;
+
+      document.getElementById("itemName").value = 1
+      document.getElementById("category").value = 2
+      document.getElementById("quantity").value =3
+      document.getElementById("price").value = 4
+
+      let myId = this.getAttribute("myId");
+      array.forEach(function (item) {
+        if (item.id == myId) {
+          item.name = document.getElementById("itemName").value;
+          item.category = document.getElementById("category").value;
+          item.quantity = document.getElementById("quantity").value;
+          item.price=document.getElementById("price").value;
+        }
+      });
+
+    }
+    editTd.append(button);
+    tr.append(editTd);
+
+    let td = document.createElement("td");
+    let deleteButton = document.createElement("button");
+    button.setAttribute("myId", index);
+    deleteButton.innerHTML = "Delete";
+    deleteButton.onclick = function () {
+      let myId = this.getAttribute("myId")
+      console.log(this.getAttribute("myId"));
+      array = array.filter((item) => item.id != myId);
+
+      console.log(this.parentNode.parentNode);
+      this.parentNode.parentNode.remove();
+
+    }
+    td.append(deleteButton);
+    tr.append(td);
+
+  
+    for(let list of array){
+      let item=list.document.getElementById("price").price;
+  ;
+       if(isNaN(item)){
+        turnRed(item)
+             }
+       }
+
+
+
+
+  }
+  function filter() {
+    console.log(array);
+    let filteredArray = array.filter((item) => Number(item.quantity) > 100);
+    let children = document.getElementById('items').childNodes;
+    for (let i = 2; i < children.length; i++) {
+      let eachTr = children[i];
+      console.log(eachTr.children[2]);
+      if (Number(eachTr.children[2].innerHTML) < 100) {
+        eachTr.style.display = "none";
+      }
+
+    }
+  }
+  // }
+  // filter by rating
+  // function filter() {
+  //   console.log(array);
+  //   let filteredArray = array.filter((item) => Number(item.rate)>4);
+     
+  //   let children = document.getElementById('items').childNodes;
+  //   for (let i = 4; i < children.length; i++){
+  //     let eachTr = children[i];
+  //     console.log(eachTr.children[4]);
+  //     if (Number(eachTr.children[4].innerHTML)<4) {
+  //       eachTr.style.display = "none";
+  //     }
+
+  //   }
+  // }
+
+
+  function clearFilter() {
+    console.log(array);
+    let filteredArray = array.filter((item) => Number(item.quantity) > 100);
+    let children = document.getElementById('items').childNodes;
+    for (let i = 2; i <body.children.length; i++) {
+      let eachTr = children[i];
+      console.log(eachTr.children[2]);
+      eachTr.style.display = "";
+    }
+    
+  }
+
+
+  function turnRed(infield){
+    infield.style.backgroundColor="red";
+      
+  }
+
+  
